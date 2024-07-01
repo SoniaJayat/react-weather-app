b62e68544e195b5b52539a2004feec1ee0e25339
SoniaJayat <SoniaJayat03@gmail.com>
2024-07-01 18:07:08 +0100

    Created Weather Forecast Header

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+export default function Header() {
+  return (
+    <header>
+      <form className="search-form" id="search-form">
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          required
+          id="search-form-input"
+          className="search-form-input"
+        />
+        <input type="submit" value="Search" className="search-form-button" />
+      </form>
+    </header>
+  );
+}
